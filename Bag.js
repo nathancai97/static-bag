ac78ea51927530be6d7f8acc133cc5a080bd2580
Nathan Cai <nathan97cai@gmail.com>
2023-02-25 21:30:45 -0500

    completed

--- Bag.js
+++ Bag.js
@@ -1,8 +1,11 @@
 class Bag {
+    static maxKg = 23;
     constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
-        } 
+        } else if(weight > Bag.maxKg) {
+            throw new Error(`Bag is too heavy!`);
+        }
         this.weight = weight
     }
 }
--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,15 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        const bag = new Bag(10)
+        expect(bag.weight).toBe(10)
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
+    })
+
+    test('should throw an error if the bag is too heavy', () => {
+        expect(() => new Bag(24)).toThrow('Bag is too heavy!');
     })
 })
